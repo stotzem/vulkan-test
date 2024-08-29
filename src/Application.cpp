6aa30a968f0f9df58f9f850dc30777b9cf01bfc1
Oliver Stotzem <5693011+stotzem@users.noreply.github.com>
2024-08-30 00:43:33 +0200

    Add Vulkan instance

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,8 +1,12 @@
 #include <SDL.h>
+#include <SDL_vulkan.h>
 #include <iostream>
+#include <vector>
+#include <vulkan/vulkan.h>
 
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "UnusedParameter"
+
 int main(int argc, char **argv) {
 #pragma clang diagnostic pop
 
@@ -11,15 +15,43 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    SDL_Window *window = SDL_CreateWindow(
-            "vulkan-test",
-            SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-            1280, 800,
-            SDL_WINDOW_VULKAN);
+    SDL_Window *window = SDL_CreateWindow("game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                          1280, 800, SDL_WINDOW_VULKAN);
     if (!window) {
         std::cerr << "Error creating window: " << SDL_GetError() << "\n";
+        SDL_Quit();
         return 1;
     }
+
+    uint32_t extensionCount = 0;
+    if (!SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, nullptr)) {
+        std::cerr << "Error at getting amount of Vulkan extensions: " << SDL_GetError() << "\n";
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+    std::vector<const char *> enabledExtensions(extensionCount);
+    if (!SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, enabledExtensions.data())) {
+        std::cerr << "Error at getting Vulkan extensions: " << SDL_GetError() << "\n";
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    std::vector<const char *> enabledLayers = {"VK_LAYER_KHRONOS_validation"};
+
+    VkApplicationInfo applicationInfo = {VK_STRUCTURE_TYPE_APPLICATION_INFO};
+    applicationInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo = {VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO};
+    createInfo.pApplicationInfo = &applicationInfo;
+    createInfo.enabledLayerCount = static_cast<uint32_t>(enabledLayers.size());
+    createInfo.ppEnabledLayerNames = enabledLayers.data();
+    createInfo.enabledExtensionCount = extensionCount;
+    createInfo.ppEnabledExtensionNames = enabledExtensions.data();
+
+    VkInstance instance;
+    vkCreateInstance(&createInfo, nullptr, &instance);
 
     bool running = true;
     while (running) {
@@ -35,6 +67,7 @@ int main(int argc, char **argv) {
         }
     }
 
+    vkDestroyInstance(instance, nullptr);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
